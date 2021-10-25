9b8c61f2f1fdf14b03892ed16f01074cd238d742
System Administrator <root@taejin-MacBookPro.local>
2021-10-26 01:39:25 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 declare const module: any;
 
 async function bootstrap() {
+  console.log('서버가 시작되었어요');
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
